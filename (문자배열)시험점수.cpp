70be269e50e8c7230530a5cd3713c4c7f9e36831
Nzukei <53482136+Doching@users.noreply.github.com>
2022-05-24 10:00:05 +0900

    Add files via upload

---
+++ (문자배열)시험점수.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main() {
+	int m[ ]={1, 3, 4, 5};
+	int n[ ]={1, 2, 4, 5};
+	int a, sum = 0;
+	for(a=0; a<=3; a++) {
+		if(m[a] == n[a])
+		sum = sum + 5;
+	}
+	printf("%d", sum);
+}
