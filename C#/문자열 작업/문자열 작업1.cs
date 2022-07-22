015f4ed6997dc58ca9073613b7d31bcd9dbcbc40
N図形 <53482136+Nzukei@users.noreply.github.com>
2022-07-22 16:08:12 +0900

    문자열 작업
    중괄호 사이에는 단일 변수로 제한되지 않는다

---
+++ C#/문자열 작업/문자열 작업1.cs
@@ -0,0 +1,11 @@
+using System;
+
+class WorkWithStrings
+{
+    static void Main()
+    {
+        string firstFriend = "Maria";
+        string secondFriend = "Sage";
+        Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+    }
+}
